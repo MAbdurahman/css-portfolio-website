072f005cf616f16091913f9858138cab541300e0
MAbdurahman <mdbdrrhm@gmail.com>
2022-06-20 09:10:35 -0500

    Add structure, styles, and responsiveness to main nav

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+/* =============================================
+            Navigation
+================================================ */
+$(document).ready(function() {
+   const hamburgerContainer = document.querySelector('#header__main-nav');
+	const hamburger = document.querySelector('.header__main-nav--hamburger');
+	const links = document.querySelectorAll('.header__main-nav--links li');
+   const body = document.querySelector('body');
+
+	hamburger.addEventListener('click', () => {
+		hamburgerContainer.classList.toggle('opened');
+      body.classList.toggle('no-scroll');
+		links.forEach(link => {
+			link.classList.toggle('opened');
+		});
+	});
+   
+
+});
+
+
+
+/**
+ * document
+    .querySelectorAll('.nav__item a[href^="#"]')
+    .forEach(trigger => {
+        trigger.onclick = function(e) {
+            e.preventDefault();
+            let hash = this.getAttribute('href');
+            let target = document.querySelector(hash);
+            let headerOffset = 100;
+            let elementPosition = target.offsetTop;
+            let offsetPosition = elementPosition - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: "smooth"
+            });
+        };
+    });
+
+ */
