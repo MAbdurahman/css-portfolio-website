02d908ef685da2b90dadb2a19b958cf2e850d849
MAbdurahman <mdbdrrhm@gmail.com>
2022-06-20 20:12:30 -0500

    Add structure, styles, animation, and responsiveness to showcase or hero section

--- js/scripts.js
+++ js/scripts.js
@@ -45,4 +45,28 @@ $(document).ready(function() {
         };
     });
 
+
+
+
+
+
+
+
+
+    const links = document.querySelectorAll(".page-header ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
  */
