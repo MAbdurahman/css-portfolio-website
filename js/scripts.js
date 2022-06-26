19cbb377dbc48fb5f63b82b13f477d0763c75f8c
MAbdurahman <mdbdrrhm@gmail.com>
2022-06-26 08:57:30 -0500

    Add a tenative solution for testimonial section

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,20 @@
-
-
-
-
-
-
 /* =============================================
             Navigation
 ================================================ */
-$(document).ready(function() {
-   const hamburgerContainer = document.querySelector('#header__main-nav');
+$(document).ready(function () {
+	const hamburgerContainer = document.querySelector('#header__main-nav');
 	const hamburger = document.querySelector('.header__main-nav--hamburger');
 	const links = document.querySelectorAll('.header__main-nav--links li');
-   const body = document.querySelector('body');
+	const body = document.querySelector('body');
 
 	hamburger.addEventListener('click', () => {
 		hamburgerContainer.classList.toggle('opened');
-      body.classList.toggle('no-scroll');
+		body.classList.toggle('no-scroll');
 		links.forEach(link => {
 			link.classList.toggle('opened');
 		});
 	});
-   
-
 });
-
-
 
 /**
  * document
@@ -70,3 +60,66 @@ function clickHandler(e) {
   });
 }
  */
+
+/*=============================================
+         testimonials
+================================================*/
+//   all ------------------
+function initParadoxWay() {
+	'use strict';
+
+	if ($('.testimonials__carousel').length > 0) {
+		var j2 = new Swiper('.testimonials__carousel .swiper-container', {
+			preloadImages: false,
+			slidesPerView: 1,
+			spaceBetween: 20,
+			loop: true,
+			grabCursor: false,
+			mousewheel: false,
+			centeredSlides: true,
+			pagination: {
+				el: '.tc-pagination',
+				clickable: true,
+				dynamicBullets: true,
+			},
+			navigation: {
+				nextEl: '.listing__carousel__button--next',
+				prevEl: '.listing__carousel__button--prev',
+			},
+			breakpoints: {
+				1024: {
+					slidesPerView: 3,
+				},
+			},
+		});
+	}
+
+	// bubbles -----------------
+
+	setInterval(function () {
+		var size = 10;
+		$('.bubbles').append(
+			'<div class="individual-bubble" style="left: ' +
+				20 +
+				'px; width: ' +
+				size +
+				'px; height:' +
+				size +
+				'px;"></div>'
+		);
+		$('.individual-bubble').animate(
+			{
+				bottom: '100%',
+				opacity: '-=0.7',
+			},
+			4000,
+			function () {
+				$(this).remove();
+			}
+		);
+	}, 350);
+}
+
+$(document).ready(function () {
+	initParadoxWay();
+});
